340845fe6a62f72650df0cdd55c9cfc41190dad0
riyafadadu04 <riyafadadu2004@gmail.com>
2024-09-02 15:35:07 +0530

    Program-10 Done

--- program-10.js
+++ program-10.js
@@ -2,6 +2,28 @@
 
 // Input: "This is a demo String find the largest word from it"
 // Output: "largest"
+function findLongestWord(str) {
+    let longestWord = "";
+    let currentWord = "";
 
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === " ") {
+        if (currentWord.length > longestWord.length) {
+          longestWord = currentWord;
+        }
+        currentWord = "";
+      } else {
+        currentWord += str[i];
+      }
+    }
 
+    if (currentWord.length > longestWord.length) {
+      longestWord = currentWord;
+    }
 
+    return longestWord;
+  }
+
+  const string = "This is a demo String find the largest word from it";
+
+  console.log(findLongestWord(string));
