54b9da9e1aabb245e1f6c1b2a49e6aeabec91a68
riyafadadu04 <riyafadadu2004@gmail.com>
2024-09-02 13:15:57 +0530

    Program-4 Done

--- program-4.js
+++ program-4.js
@@ -13,3 +13,30 @@
 
 // Output: 1 (index of an object for prop_1: 'val_4')
 
+function getIndexByProperty(array, property, value) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i][property] === value) {
+        return i;
+      }
+    }
+  }
+
+  const arrayObj = [
+    {
+      prop_1: "val_1",
+      prop_2: "val_2",
+      prop_3: "val_3",
+    },
+    {
+      prop_1: "val_4",
+      prop_2: "val_5",
+      prop_3: "val_6",
+    },
+  ];
+
+  const property = "prop_1";
+  const value = "val_4";
+
+  const index = getIndexByProperty(arrayObj, property, value);
+  
+  console.log(`index of an object for ${property}: '${value}' = ${index}`);
