47a83d9a698465b6316f9f93aa92dbf0d1faef31
riyafadadu04 <riyafadadu2004@gmail.com>
2024-09-02 14:51:44 +0530

    Program-6 Done

--- program-6.js
+++ program-6.js
@@ -22,3 +22,30 @@
 //     { employee_id: 2, employee_name: 'Rahul' },
 //     { employee_id: 3, employee_name: 'Chaitanya' }
 // ] 
+
+function countOccurrences(array, property, oldVal , newVal) {
+    array.forEach((obj) => {
+      if(obj[property] === oldVal){
+        obj[property] = newVal;
+      }
+    });
+    return array;
+  }
+
+  const arr = [
+    {
+      employee_id: 1,
+      employee_name: "Aman",
+    },
+    {
+      employee_id: 2,
+      employee_name: "Bhargava",
+    },
+    {
+      employee_id: 3,
+      employee_name: "Chaitanya",
+    },
+  ];
+
+  console.log(countOccurrences(arr, "employee_name"  , "Bhargava" , "Rahul"));
+  // console.log(countOccurrences(arr, "employee_id"  , 1 , 5));
