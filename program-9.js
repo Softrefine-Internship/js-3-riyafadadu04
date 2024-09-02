eba24a86bf551d499810bc358c4d8cef9413d150
riyafadadu04 <riyafadadu2004@gmail.com>
2024-09-02 15:20:53 +0530

    Program-9 Done

--- program-9.js
+++ program-9.js
@@ -3,5 +3,16 @@
 // Input: Dog 
 // Output: [ 'd', 'do', 'dog', 'o', 'og', 'g' ]
 
+function generateCombinations(str) {
+    const result = [];
+    for (let i = 0; i < str.length; i++) {
+      for (let j = i + 1; j <= str.length; j++) {
+        result.push(str.toLowerCase().slice(i, j));
+      }
+    }
+    return result;
+  }
 
+  const string = "Dog";
 
+  console.log(generateCombinations(string));
