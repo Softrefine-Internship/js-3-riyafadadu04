e00bb7fc2ccd9b0046a6ec61f5d9894fba7f1f37
riyafadadu04 <riyafadadu2004@gmail.com>
2024-09-02 16:34:45 +0530

    Program-16 Done

--- program-16.js
+++ program-16.js
@@ -5,3 +5,20 @@
 // let set2 = new Set([3, 4, 5, 6]);
 
 // Output: [3, 4]
+
+function checkIntersection(set1, set2) {
+    const result = [];
+
+    for (const element of set1) {
+      if (set2.has(element)) {
+        result.push(element);
+      }
+    }
+
+    return result;
+  }
+  
+  let set1 = new Set([1, 2, 3, 4]);
+  let set2 = new Set([3, 4, 5, 6]);
+  
+  console.log(checkIntersection(set1, set2));
