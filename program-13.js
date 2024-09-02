05f6f8028a1c72f3d0b5b045b579e096ff589aad
riyafadadu04 <riyafadadu2004@gmail.com>
2024-09-02 16:13:16 +0530

    Program-13 Done

--- program-13.js
+++ program-13.js
@@ -10,3 +10,27 @@
 
 // Output:
 // { 'John' => [ 1 ], 'Alice' => [ 2, 4 ], 'Bob' => [ 3 ] }
+
+function groupByProperty(arr, property) {
+    const result = {};
+
+    for (let i = 0; i < arr.length; i++) {
+      const key = arr[i][property];
+      if (result[key]) {
+        result[key].push(arr[i].id);
+      } else {
+        result[key] = [arr[i].id];
+      }
+    }
+    
+    return result;
+  }
+
+  const arr = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Alice" },
+    { id: 3, name: "Bob" },
+    { id: 4, name: "Alice" },
+  ];
+
+  console.log(groupByProperty(arr, "name"));
