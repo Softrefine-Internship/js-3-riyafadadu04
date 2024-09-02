0a80174508e188e2f3578a9a500201c834dc9914
riyafadadu04 <riyafadadu2004@gmail.com>
2024-09-02 15:51:42 +0530

    Program-11 Done

--- program-11.js
+++ program-11.js
@@ -11,3 +11,18 @@
 // Example 3:
 // Input: eleven plus two = twelve plus one
 // Output: true
+
+function areAnagrams(str1, str2) {
+  const sortedStr1 = str1.split("").sort().join("");
+  const sortedStr2 = str2.split("").sort().join("");
+
+  return sortedStr1 === sortedStr2;
+}
+
+const str1 = "eleven plus two";
+const str2 = "twelve plus one";
+const str3 = "evil";
+const str4 = "vile";
+
+console.log(areAnagrams(str1, str2));
+console.log(areAnagrams(str3, str4));
