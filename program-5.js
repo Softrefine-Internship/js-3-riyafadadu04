9afa0c3ba69b0390b8de9411d1e6a35dd4c7900c
riyafadadu04 <riyafadadu2004@gmail.com>
2024-09-02 13:34:41 +0530

    Program-5 Done

--- program-5.js
+++ program-5.js
@@ -1,6 +1,6 @@
 // Write a JavaScript program to count number of occurrences of repeated names in an array of objects.
 
-// Input:  
+// Input:
 // arr = [
 //     {
 //        employeeName: "Ram",
@@ -28,10 +28,55 @@
 //     }
 //  ]
 
-// Output: 
+// Output:
 // [
 //     {employeeName: "Ram", occurrences: 3},
 //     {employeeName: "Shyam", occurrences: 2},
 //     {employeeName: "Kisan", occurrences:  1}
-// ] 
+// ]
 
+function countOccurrences(array, property) {
+  const result = [];
+  const values = new Set();
+  array.forEach((obj) => {
+    if (!values.has(obj[property])) {
+      values.add(obj[property]);
+      result.push({
+        [property]: obj[property],
+        occurrences: 1,
+      });
+    } else {
+      result.find((r) => r[property] === obj[property]).occurrences++;
+    }
+  });
+  return result;
+}
+
+const arr = [
+  {
+    employeeName: "Ram",
+    employeeId: 23,
+  },
+  {
+    employeeName: "Shyam",
+    employeeId: 24,
+  },
+  {
+    employeeName: "Ram",
+    employeeId: 21,
+  },
+  {
+    employeeName: "Ram",
+    employeeId: 25,
+  },
+  {
+    employeeName: "Kisan",
+    employeeId: 22,
+  },
+  {
+    employeeName: "Shyam",
+    employeeId: 20,
+  },
+];
+
+console.log(countOccurrences(arr, "employeeName"));
