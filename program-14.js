bd30e183f239b6ded6aab4f798c5e937e9902269
riyafadadu04 <riyafadadu2004@gmail.com>
2024-09-02 16:20:03 +0530

    Program-14 Done

--- program-14.js
+++ program-14.js
@@ -2,3 +2,21 @@
 
 // Input: "hello"
 // Output: { 'h': 1, 'e': 1, 'l': 2, 'o': 1 }
+
+function frequencyCounter(str) {
+    const frequencyMap = {};
+    const newStr = str.replaceAll(" ", "").toLowerCase();
+
+    for (const char of newStr) {
+      if (frequencyMap[char]) {
+        frequencyMap[char]++;
+      } else {
+        frequencyMap[char] = 1;
+      }
+    }
+
+    return frequencyMap;
+  }
+
+  const string = "hello";
+  console.log(frequencyCounter(string));
