8e23b79e8130112d3d5a1bf783682b2803c1db0f
riyafadadu04 <riyafadadu2004@gmail.com>
2024-09-02 15:56:49 +0530

    Program-12 Done

--- program-12.js
+++ program-12.js
@@ -13,4 +13,23 @@
 // Output: "JavaScript"
 
 
+function findLongestString(arr) {
+    let longestString = "";
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length > longestString.length) {
+        longestString = arr[i];
+      }
+    }
+    return longestString;
+  }
 
+  const arr = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "C++",
+    "Ruby",
+    "Swift",
+  ];
+  
+  console.log(findLongestString(arr));
